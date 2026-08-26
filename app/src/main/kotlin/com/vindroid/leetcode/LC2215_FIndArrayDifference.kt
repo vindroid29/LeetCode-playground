f8fea2f717d925126951d38c9c2cc75383af91a1
vinay kumbhar <vinay.pkumbhar@gmail.com>
2026-08-26 17:16:54 +0530

    added new leetcode examples

--- app/src/main/kotlin/com/vindroid/leetcode/LC2215_FIndArrayDifference.kt
+++ app/src/main/kotlin/com/vindroid/leetcode/LC2215_FIndArrayDifference.kt
@@ -1,2 +1,28 @@
 package com.vindroid.leetcode
 
+fun main() {
+    val nums1 = intArrayOf(1,2,3)
+    val nums2 = intArrayOf(2,4,6)
+    val result = findDifference(nums1,nums2)
+    println("Input : num1 :${nums1.contentToString()} num2: ${nums2.contentToString()}")
+    println("Output : $result")
+}
+
+private fun findDifference(nums1: IntArray, nums2: IntArray): List<List<Int>> {
+    val set1 = nums1.toHashSet()
+    val set2 = nums2.toHashSet()
+    val result1 = mutableListOf<Int>()
+    val result2 = mutableListOf<Int>()
+
+    for (num in nums1.toSet()){
+        if(num !in set2){
+            result1.add(num)
+        }
+    }
+    for (num in nums2.toSet()){
+        if(num !in set1){
+            result2.add(num)
+        }
+    }
+    return listOf(result1,result2)
+}
